97d29924d5b40fe3e80e6907816c51279b88910a
Paul Nozdrachev <p.nozdra4ev@yandex.ru>
2024-02-19 23:12:13 +0300

    .

--- main.go
+++ main.go
@@ -1,29 +1,9 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	jobsFlow := []job{
+	flow := []job{
+		producer,
 		SingleHash,
-		MultiHash,
-		CombineResults,
 	}
-	ExecutePipeline(jobsFlow...)
-	fmt.Scanln()
-}
-
-func receiver(in chan interface{}, out chan interface{}) {
-	for val := range in {
-		fmt.Println(val)
-	}
-}
-
-func generator(in chan interface{}, out chan interface{}) {
-	inputData := []int{0, 1, 1, 2, 3, 5, 8}
-	for _, val := range inputData {
-		out <- val
-	}
-	close(out)
+	ExecutePipeline(flow...)
 }
--- signer.go
+++ signer.go
@@ -1,108 +1,64 @@
 package main
 
 import (
-	"sort"
+	"fmt"
 	"strconv"
-	"strings"
 	"sync"
+	"time"
 )
 
-// сюда писать код
-
-// пришло очередное значение - запускается конвейер
-// и так до тех пор, пока есть значения
-// если значений больше нет, надо прервать выполнение функции
-// сделать это через канал
-// значение отправляет производитель
 func ExecutePipeline(jobs ...job) {
+	wg := &sync.WaitGroup{}
 	in := make(chan interface{})
 	out := make(chan interface{})
-	wg := &sync.WaitGroup{}
-	wg.Add(1)
-	for _, joba := range jobs {
+	for _, j := range jobs {
+		wg.Add(1)
 		go func(j job) {
+			defer wg.Done()
 			j(in, out)
-		}(joba)
+		}(j)
 		in = out
 		out = make(chan interface{})
 	}
-	go func() {
-		wg.Wait()
-	}()
+	wg.Wait()
+}
+
+func producer(in chan interface{}, out chan interface{}) {
+	inputData := []int{0, 1, 1, 2, 3, 5}
+	for _, val := range inputData {
+		out <- val
+		time.Sleep(time.Millisecond * 10)
+	}
+	close(out)
 }
 
 // crc32(data)+"~"+crc32(md5(data))
 func SingleHash(in chan interface{}, out chan interface{}) {
-	for rawVal := range in {
-		val, ok := rawVal.(int)
+	for rawData := range in {
+		num, ok := rawData.(int)
 		if ok {
-			ch1 := make(chan string)
-			ch2 := make(chan string)
+			data := strconv.Itoa(num)
+			c := make(chan string, 1) // этот канал связывает две горутины: первая вычисляет md5, вторая вычисляет crc32 на основе md5
 
-			md5Hash := DataSignerMd5(strconv.Itoa(val))
-
-			// запускаем горутину для подсчета crc32(data)
-			go func(ch1 chan string) {
-				ch1 <- DataSignerCrc32(strconv.Itoa(val))
-			}(ch1)
-
-			// запускаем горутину для подсчета crc32 из хеша
-			go func(ch2 chan string) {
-				ch2 <- DataSignerCrc32(md5Hash)
-			}(ch2)
-			// получаем результаты из горутин
-			out <- (<-ch1 + "~" + <-ch2)
-		}
-	}
-	close(out)
-}
-
-// crc32(th+data)
-func MultiHash(in chan interface{}, out chan interface{}) {
-	type pair struct {
-		th   int
-		data string
-	}
-
-	for rawVal := range in {
-		data, ok := rawVal.(string)
-		if ok {
-			wg := &sync.WaitGroup{}
-			wg.Add(6)
-			results := make(chan pair)
-
-			// запускаем цикл из 6 горутин для вычисления хеша
-			for i := 0; i < 6; i++ {
-				go func(th int, singleHashString string) {
-					defer wg.Done()
-					hash := DataSignerCrc32(strconv.Itoa(th) + singleHashString)
-					results <- pair{th, hash}
-				}(i, data)
-			}
 			go func() {
-				wg.Wait()
-				close(results)
+				res := DataSignerCrc32(data)
+				fmt.Printf("crc32: %v\n", res)
+				out <- res
 			}()
 
-			res := make([]string, 6)
-			for r := range results {
-				res[r.th] = r.data
-			}
-			out <- strings.Join(res, "")
+			go func(output chan string) {
+				output <- DataSignerMd5(data)
+				close(output)
+			}(c)
+			go func(input chan string) {
+				res := DataSignerCrc32(<-input)
+				fmt.Printf("HashCrc32: %v\n", res)
+				out <- res
+			}(c)
 		}
 	}
 	close(out)
 }
 
-func CombineResults(in chan interface{}, out chan interface{}) {
-	results := make([]string, 0)
-	for rawVal := range in {
-		data, ok := rawVal.(string)
-		if ok {
-			results = append(results, data)
-		}
-	}
-	sort.Slice(results, func(i, j int) bool { return results[i] < results[j] })
-	out <- strings.Join(results, "_")
-	close(out)
-}
+func MultiHash(in chan interface{}, out chan interface{})      {}
+func CombineResults(in chan interface{}, out chan interface{}) {}
