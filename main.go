7ff6bafff9bba74e02110afa3c2faf74f99543dd
Paul Nozdrachev <p.nozdra4ev@yandex.ru>
2024-02-19 18:15:59 +0300

    .

--- main.go
+++ main.go
@@ -1,9 +1,29 @@
 package main
 
-import "time"
+import (
+	"fmt"
+)
 
 func main() {
-	pipeline := []job{gen, SingleHash, MultiHash, CombineResults}
-	ExecutePipeline(pipeline...)
-	time.Sleep(time.Second * 20)
+	jobsFlow := []job{
+		SingleHash,
+		MultiHash,
+		CombineResults,
+	}
+	ExecutePipeline(jobsFlow...)
+	fmt.Scanln()
+}
+
+func receiver(in chan interface{}, out chan interface{}) {
+	for val := range in {
+		fmt.Println(val)
+	}
+}
+
+func generator(in chan interface{}, out chan interface{}) {
+	inputData := []int{0, 1, 1, 2, 3, 5, 8}
+	for _, val := range inputData {
+		out <- val
+	}
+	close(out)
 }
--- signer.go
+++ signer.go
@@ -1,81 +1,108 @@
 package main
 
 import (
-	"bytes"
-	"fmt"
-	"math/rand"
 	"sort"
 	"strconv"
 	"strings"
+	"sync"
 )
 
 // сюда писать код
-func ExecutePipeline(j ...job) {
-	for _, task := range j {
-		in := make(chan interface{})
-		out := make(chan interface{})
-		go task(in, out)
+
+// пришло очередное значение - запускается конвейер
+// и так до тех пор, пока есть значения
+// если значений больше нет, надо прервать выполнение функции
+// сделать это через канал
+// значение отправляет производитель
+func ExecutePipeline(jobs ...job) {
+	in := make(chan interface{})
+	out := make(chan interface{})
+	wg := &sync.WaitGroup{}
+	wg.Add(1)
+	for _, joba := range jobs {
+		go func(j job) {
+			j(in, out)
+		}(joba)
+		in = out
+		out = make(chan interface{})
 	}
+	go func() {
+		wg.Wait()
+	}()
 }
 
-func gen(in chan interface{}, out chan interface{}) {
-	for i := 0; i < 100; i++ {
-		out <- rand.Intn(100)
-	}
-}
+// crc32(data)+"~"+crc32(md5(data))
+func SingleHash(in chan interface{}, out chan interface{}) {
+	for rawVal := range in {
+		val, ok := rawVal.(int)
+		if ok {
+			ch1 := make(chan string)
+			ch2 := make(chan string)
 
-func SingleHash(inputCh chan interface{}, outCh chan interface{}) {
+			md5Hash := DataSignerMd5(strconv.Itoa(val))
 
-	for {
-		rawData, open := <-inputCh
-		if open {
-			str, asserted := rawData.(string)
-			if asserted {
-				crc := DataSignerCrc32(str)
-				// time.Sleep(time.Millisecond * 10)
-				crcWithHash := DataSignerCrc32(DataSignerMd5(str))
-				outCh <- (crc + "~" + crcWithHash)
-			}
-		} else {
-			close(outCh)
-			break
+			// запускаем горутину для подсчета crc32(data)
+			go func(ch1 chan string) {
+				ch1 <- DataSignerCrc32(strconv.Itoa(val))
+			}(ch1)
+
+			// запускаем горутину для подсчета crc32 из хеша
+			go func(ch2 chan string) {
+				ch2 <- DataSignerCrc32(md5Hash)
+			}(ch2)
+			// получаем результаты из горутин
+			out <- (<-ch1 + "~" + <-ch2)
 		}
 	}
+	close(out)
 }
 
-func MultiHash(inputCh chan interface{}, outCh chan interface{}) {
-	for {
-		rawData, open := <-inputCh
-		if open {
-			singHash, asserted := rawData.(string)
-			if asserted {
-				buffer := bytes.NewBuffer(make([]byte, 0))
-				for i := 0; i < 6; i++ {
-					buffer.Write([]byte(DataSignerCrc32(strconv.Itoa(i) + singHash)))
-				}
-				outCh <- buffer.String()
+// crc32(th+data)
+func MultiHash(in chan interface{}, out chan interface{}) {
+	type pair struct {
+		th   int
+		data string
+	}
+
+	for rawVal := range in {
+		data, ok := rawVal.(string)
+		if ok {
+			wg := &sync.WaitGroup{}
+			wg.Add(6)
+			results := make(chan pair)
+
+			// запускаем цикл из 6 горутин для вычисления хеша
+			for i := 0; i < 6; i++ {
+				go func(th int, singleHashString string) {
+					defer wg.Done()
+					hash := DataSignerCrc32(strconv.Itoa(th) + singleHashString)
+					results <- pair{th, hash}
+				}(i, data)
 			}
-		} else {
-			close(outCh)
-			break
+			go func() {
+				wg.Wait()
+				close(results)
+			}()
+
+			res := make([]string, 6)
+			for r := range results {
+				res[r.th] = r.data
+			}
+			out <- strings.Join(res, "")
 		}
 	}
+	close(out)
 }
 
-func CombineResults(inputCh chan interface{}, outCh chan interface{}) {
-	results := make([]string, 100) // делаем размер слайса 100, чтобы не тратить время на пересоздание
-	for {
-		rawData, open := <-inputCh
-		if open {
-			if res, asserted := rawData.(string); asserted {
-				results = append(results, res)
-			}
-		} else {
-			close(outCh) // подумать над тем, кто будет закрывать выходной канал
-			// входной канал для первой функции может быть выходным для второй
-			break
+func CombineResults(in chan interface{}, out chan interface{}) {
+	results := make([]string, 0)
+	for rawVal := range in {
+		data, ok := rawVal.(string)
+		if ok {
+			results = append(results, data)
 		}
 	}
 	sort.Slice(results, func(i, j int) bool { return results[i] < results[j] })
-	fmt.Println(strings.Join(results, "_"))
+	out <- strings.Join(results, "_")
+	close(out)
 }
