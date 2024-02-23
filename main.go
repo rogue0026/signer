8e58fb29b91b18c691512d1e03dfa326ac360f51
Paul Nozdrachev <p.nozdra4ev@yandex.ru>
2024-02-23 14:32:07 +0300

    Распараллелил SingleHash и избавился от datarace

--- main.go
+++ main.go
@@ -3,16 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	c1 := make(chan interface{}, MaxInputDataLen)
-	c2 := make(chan interface{}, MaxInputDataLen)
-	c3 := make(chan interface{}, MaxInputDataLen)
-	c4 := make(chan interface{}, MaxInputDataLen)
-	c5 := make(chan interface{}, MaxInputDataLen)
-	go Producer(c1, c2)
-	go SingleHash(c2, c3)
-	go MultiHash(c3, c4)
-	go CombineResults(c4, c5)
-	for val := range c5 {
-		fmt.Println(val)
+	c1 := make(chan interface{}, 10)
+	c2 := make(chan interface{}, 10)
+	inpData := []int{0, 1, 1, 2, 3, 5, 8}
+	go func() {
+		for _, elem := range inpData {
+			c1 <- elem
+		}
+		close(c1)
+	}()
+	SingleHash(c1, c2)
+
+	for res := range c2 {
+		fmt.Println(res)
 	}
 }
--- signer.go
+++ signer.go
@@ -1,66 +1,43 @@
 package main
 
 import (
-	"sort"
 	"strconv"
-	"strings"
+	"sync"
 )
 
 // сюда писать код
-var jobsFlow = []job{
-	Producer,
-	SingleHash,
-	MultiHash,
-	CombineResults,
-}
 
-func ExecutePipeline(jobs ...job) {
-	//for i := 0; i < len(jobs); i++ {
-	//	jobs[i]()
-	//}
-}
-
-func Producer(in, out chan interface{}) {
-	inputData := []int{0, 1, 1, 2, 3, 5, 8}
-	for _, elem := range inputData {
-		out <- elem
-	}
-	close(out)
-}
-
-func SingleHash(in, out chan interface{}) {
+// crc(data) + crc(hash(data))
+var SingleHash = func(in, out chan interface{}) {
+	wg := sync.WaitGroup{}
+	//m := sync.Mutex{}
 	for val := range in {
-		if num, ok := val.(int); ok {
-			hash := DataSignerMd5(strconv.Itoa(num))
-			hashCRC := DataSignerCrc32(hash)
-			dataCRC := DataSignerCrc32(strconv.Itoa(num))
-			out <- dataCRC + "~" + hashCRC
+		if n, ok := val.(int); ok {
+			data := strconv.Itoa(n)
+			hashData := DataSignerMd5(data)
+
+			wg.Add(1)
+			go func(normalData string, hash string) {
+				defer wg.Done()
+				hashCRCCh := make(chan string, 1)
+				dataCRCCh := make(chan string, 1)
+				go func(hashCRCCh chan string) {
+					hashCRC := DataSignerCrc32(hash)
+					hashCRCCh <- hashCRC
+				}(hashCRCCh)
+				go func(dataCRCCh chan string) {
+					dataCRC := DataSignerCrc32(normalData)
+					dataCRCCh <- dataCRC
+				}(dataCRCCh)
+				dCRC := <-dataCRCCh
+				hCRC := <-hashCRCCh
+				out <- dCRC + "~" + hCRC
+			}(data, hashData)
 		}
 	}
+	wg.Wait()
 	close(out)
+	// дождаться пока все отправят данные
 }
-
-func MultiHash(in, out chan interface{}) {
-	for val := range in {
-		var multiHashVal string
-		if singHash, ok := val.(string); ok {
-			for i := 0; i < 6; i++ {
-				multiHashVal += DataSignerCrc32(strconv.Itoa(i) + singHash)
-			}
-			out <- multiHashVal
-		}
-	}
-	close(out)
-}
-
-func CombineResults(in, out chan interface{}) {
-	multiHashResults := make([]string, 0, MaxInputDataLen)
-	for val := range in {
-		if multiHashRes, ok := val.(string); ok {
-			multiHashResults = append(multiHashResults, multiHashRes)
-		}
-	}
-	sort.Slice(multiHashResults, func(i int, j int) bool { return multiHashResults[i] < multiHashResults[j] })
-	out <- strings.Join(multiHashResults, "_")
-	close(out)
-}
+var MultiHash = func(in, out chan interface{}) {}
+var CombineResults = func(in, out chan interface{}) {}
